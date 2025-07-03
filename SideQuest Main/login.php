62e6067979cfe8f0a1392af7eb39f0756736f30a
Darryl Baranda <84567055+Oralcee@users.noreply.github.com>
2025-07-03 21:52:55 +0800

    Main

---
+++ SideQuest Main/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="home.php" method="post">
+        <h2>Login</h2>
+        <div class="container">
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter your username" name="username" required><br>
+
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter your password" name="password" required><br>
+
+            <button type="submit">Login</button>
+        </div>
+        <div class="container">
+        </div>
+    </form>
+</body>
+</html>
